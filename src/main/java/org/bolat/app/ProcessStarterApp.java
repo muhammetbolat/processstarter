f681d1f5a63bebf5ddf2719d1b918df78050b9b5
bolat <muhammetbolat@gmail.com>
2022-08-11 23:00:10 +0300

    Initial commits

--- src/main/java/org/bolat/app/ProcessStarterApp.java
+++ src/main/java/org/bolat/app/ProcessStarterApp.java
@@ -1,4 +1,4 @@
-package org.csystem.app;
+package org.bolat.app;
 
 import org.csystem.util.console.Console;
 
--- src/test/java/org/bolat/app/GenericappApplicationTests.java
+++ src/test/java/org/bolat/app/GenericappApplicationTests.java
@@ -1,4 +1,4 @@
-package org.csystem.app;
+package org.bolat.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
